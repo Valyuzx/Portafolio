cae72b123344aec0bd482011bd22c1172ec9ff56
Braulio Garcia <braugg02@gmail.com>
2026-06-27 20:46:56 -0600

    Update file program.cs to add Swagger

--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -1,33 +1,56 @@
+using BACKEND.BuissnesLayer;
 using BACKEND.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager Configuration = builder.Configuration;
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options => { /* Configuración JWT */ });
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  //  .AddJwtBearer(options => { /* Configuración JWT */ });
+builder.Services.AddScoped<IAccountBL, AccountBL>();
 
 builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", p => p.WithOrigins("...").AllowAnyMethod()));
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(Configuration.GetConnectionString("ApplicationDbContext"))
 );
 
-var app = builder.Build();
+builder.Services.AddEndpointsApiExplorer();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        In = ParameterLocation.Header,
+        Description = "Ingresa el token JWT. Ejemplo: Bearer {tu_token}"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }},
+            Array.Empty<string>()
+        }
+    });
+});
+
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 
 app.UseCors("Frontend");
 app.UseHttpsRedirection();
