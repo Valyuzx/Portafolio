581b443824bda6a98e3b708a28478ad0c0347e9e
Braulio Garcia <braugg02@gmail.com>
2026-07-02 10:31:33 -0600

    add refresh token and logout into IAccountBL

--- BACKEND/BuissnesLayer/IAccountBL.cs
+++ BACKEND/BuissnesLayer/IAccountBL.cs
@@ -1,5 +1,4 @@
 ﻿using BACKEND.DTOs;
-
 namespace BACKEND.BuissnesLayer
 {
     public interface IAccountBL
@@ -7,5 +6,7 @@ namespace BACKEND.BuissnesLayer
         Task<bool> RegisterAsync(RegisterRequestDTO registerDto);
         Task<bool> RegisterAdminAsync(RegisterRequestDTO registerDto);
         Task<AuthResponseDTO?> LoginAsync(LoginRequestDTO loginDto);
+        Task<RefreshTokenResponseDTO?> RefreshTokenAsync(string refreshToken,string? ipAddress = null);
+        Task<bool> LogoutAsync(string accessToken, string? refreshToken,Guid userId);
     }
 }
